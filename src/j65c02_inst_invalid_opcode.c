4a522038ef777ca0a1758162f9850c8a99248848
Justin Handville <nanolith@gmail.com>
2022-11-10 14:51:18 -0500

    Added invalid opcode impl.

---
+++ src/j65c02_inst_invalid_opcode.c
@@ -0,0 +1,25 @@
+/**
+ * \file j65c02_inst_invalid_opcode.c
+ *
+ * \brief Handle an invalid opcode instruction.
+ *
+ * \copyright 2022 Justin Handville.  Please see LICENSE.txt in this
+ * distribution for the license terms under which this software is distributed.
+ */
+
+#include "jemu65c02_internal.h"
+
+/**
+ * \brief Handle an invalid opcode instruction by setting the crash flag.
+ *
+ * \param inst              The emulator instance on which this instruction
+ *                          executes.
+ *
+ * \returns JEMU_ERROR_INVALID_OPCODE.
+ */
+JEMU_SYM(status) JEMU_SYM(j65c02_inst_invalid_opcode)(JEMU_SYM(j65c02)* inst)
+{
+    inst->crash = true;
+
+    return JEMU_ERROR_INVALID_OPCODE;
+}
