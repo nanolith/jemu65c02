1d9301c1a936fe680d93b2b587a35b6e747d97e5
Justin Handville <nanolith@gmail.com>
2022-11-30 22:32:08 -0500

    Added ASL ZER instruction.

--- src/instructions.c
+++ src/instructions.c
@@ -28,8 +28,8 @@ JEMU_SYM(j65c02_instruction) JEMU_SYM(global_j65c02_instructions)[256] = {
     { .exec = &JEMU_SYM(j65c02_inst_invalid_opcode), .max_cycles = 2 },
     /* Opcode 0x05 - TODO implement. */
     { .exec = &JEMU_SYM(j65c02_inst_invalid_opcode), .max_cycles = 2 },
-    /* Opcode 0x06 - TODO implement. */
-    { .exec = &JEMU_SYM(j65c02_inst_invalid_opcode), .max_cycles = 2 },
+    /* Opcode 0x06 - ASL ZER instruction. */
+    { .exec = &JEMU_SYM(j65c02_inst_ASL_zer), .max_cycles = 5 },
     /* Opcode 0x07 - TODO implement. */
     { .exec = &JEMU_SYM(j65c02_inst_invalid_opcode), .max_cycles = 2 },
     /* Opcode 0x08 - TODO implement. */
