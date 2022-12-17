78abf5a8919e7825ca17262c75aad65aecf1134a
Justin Handville <nanolith@gmail.com>
2022-12-17 18:37:42 -0500

    impl of CPY imm instruction.

--- src/jemu65c02_internal.h
+++ src/jemu65c02_internal.h
@@ -977,6 +977,21 @@ JEMU_SYM(status) JEMU_SYM(j65c02_inst_CPX_abs)(
 JEMU_SYM(status) JEMU_SYM(j65c02_inst_CPX_zer)(
     JEMU_SYM(j65c02)* inst, int* cycles);
 
+/**
+ * \brief Handle a CPY IMM instruction.
+ *
+ * \param inst              The emulator instance on which this instruction
+ *                          executes.
+ * \param cycles            The number of cycles taken to execute this
+ *                          instruction.
+ *
+ * \returns a status code indicating success or failure.
+ *      - STATUS_SUCCESS on success.
+ *      - a non-zero error code on failure.
+ */
+JEMU_SYM(status) JEMU_SYM(j65c02_inst_CPY_imm)(
+    JEMU_SYM(j65c02)* inst, int* cycles);
+
 /******************************************************************************/
 /* Start of public exports.                                                   */
 /******************************************************************************/
