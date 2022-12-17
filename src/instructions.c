877197cb3c9cb3a85b9ceb334c88370719ea9352
Justin Handville <nanolith@gmail.com>
2022-12-17 14:40:30 -0500

    impl of CMP abs x instruction.

--- src/instructions.c
+++ src/instructions.c
@@ -484,8 +484,8 @@ JEMU_SYM(j65c02_instruction) JEMU_SYM(global_j65c02_instructions)[256] = {
     { .exec = &JEMU_SYM(j65c02_inst_invalid_opcode), .max_cycles = 2 },
     /* Opcode 0xDC - TODO implement. */
     { .exec = &JEMU_SYM(j65c02_inst_invalid_opcode), .max_cycles = 2 },
-    /* Opcode 0xDD - TODO implement. */
-    { .exec = &JEMU_SYM(j65c02_inst_invalid_opcode), .max_cycles = 2 },
+    /* Opcode 0xDD - CMP ABS X instruction. */
+    { .exec = &JEMU_SYM(j65c02_inst_CMP_abs_x), .max_cycles = 4 },
     /* Opcode 0xDE - TODO implement. */
     { .exec = &JEMU_SYM(j65c02_inst_invalid_opcode), .max_cycles = 2 },
     /* Opcode 0xDF - TODO implement. */
