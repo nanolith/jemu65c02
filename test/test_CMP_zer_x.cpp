8cc72d07fbd68b35af266a338d367baf59780537
Justin Handville <nanolith@gmail.com>
2022-12-17 14:56:17 -0500

    impl of CMP zer x instruction.

---
+++ test/test_CMP_zer_x.cpp
@@ -0,0 +1,259 @@
+#include <minunit/minunit.h>
+#include <jemu65c02/jemu65c02.h>
+#include <string.h>
+
+JEMU_IMPORT_jemu65c02;
+
+TEST_SUITE(j65c02_CMP_zer_x);
+
+static status mem_read(void* varr, uint16_t addr, uint8_t* val)
+{
+    const uint8_t* arr = (const uint8_t*)varr;
+
+    *val = arr[addr];
+
+    return STATUS_SUCCESS;
+}
+
+static status mem_write(void* varr, uint16_t addr, uint8_t val)
+{
+    uint8_t* arr = (uint8_t*)varr;
+
+    arr[addr] = val;
+
+    return STATUS_SUCCESS;
+}
+
+/**
+ * 0xD5 CMP ZER X (comparing two equal values).
+ */
+TEST(step_CMP_zer_x_equal)
+{
+    j65c02* inst = nullptr;
+    uint8_t mem[65536];
+    const uint8_t EXPECTED_CMP_INPUT = 0x05;
+    const uint8_t EXPECTED_A_VALUE = 0x05;
+
+    /* clear memory. */
+    memset(mem, 0, sizeof(mem));
+
+    /* set the reset vector. */
+    mem[0xFFFC] = 0x00;
+    mem[0xFFFD] = 0x10;
+
+    /* at 0x1000, add a CMP instruction. */
+    mem[0x1000] = 0xD5;
+    mem[0x1001] = 0x20;
+
+    /* at 0x0025, set the CMP input. */
+    mem[0x0025] = EXPECTED_CMP_INPUT;
+
+    /* create an instance. */
+    TEST_ASSERT(
+        STATUS_SUCCESS
+            == j65c02_create(
+                    &inst, &mem_read, &mem_write, mem,
+                    JEMU_65c02_PERSONALITY_WDC,
+                    JEMU_65c02_EMULATION_MODE_STRICT));
+
+    /* PRECONDITION: crash flag is set. */
+    TEST_EXPECT(j65c02_crash_flag_get(inst));
+
+    /* reset the processor. */
+    TEST_ASSERT(STATUS_SUCCESS == j65c02_reset(inst));
+
+    /* PRECONDITION: the zero flag is clear. */
+    j65c02_reg_status_set(
+        inst, j65c02_reg_status_get(inst) & ~JEMU_65c02_STATUS_ZERO);
+
+    /* PRECONDITION: the negative flag is set. */
+    j65c02_reg_status_set(
+        inst, j65c02_reg_status_get(inst) | JEMU_65c02_STATUS_NEGATIVE);
+
+    /* PRECONDITION: the carry flag is clear. */
+    j65c02_reg_status_set(
+        inst, j65c02_reg_status_get(inst) & ~JEMU_65c02_STATUS_CARRY);
+
+    /* PRECONDITION: set the A register. */
+    j65c02_reg_a_set(inst, EXPECTED_A_VALUE);
+
+    /* PRECONDITION: set the X register. */
+    j65c02_reg_x_set(inst, 0x05);
+
+    /* PRECONDITION: PC is 0x1000. */
+    TEST_ASSERT(0x1000 == j65c02_reg_pc_get(inst));
+
+    /* single step. */
+    TEST_ASSERT(STATUS_SUCCESS == j65c02_step(inst));
+
+    /* POSTCONDITION: PC is 0x1002. */
+    TEST_EXPECT(0x1002 == j65c02_reg_pc_get(inst));
+
+    /* POSTCONDITION: the zero flag is set. */
+    TEST_EXPECT(j65c02_reg_status_get(inst) & JEMU_65c02_STATUS_ZERO);
+
+    /* POSTCONDITION: the negative flag is clear. */
+    TEST_EXPECT(!(j65c02_reg_status_get(inst) & JEMU_65c02_STATUS_NEGATIVE));
+
+    /* POSTCONDITION: the carry flag is set. */
+    TEST_EXPECT(j65c02_reg_status_get(inst) & JEMU_65c02_STATUS_CARRY);
+
+    /* clean up. */
+    TEST_ASSERT(STATUS_SUCCESS == j65c02_release(inst));
+}
+
+/**
+ * 0xD5 CMP ZER X (comparing a lesser value).
+ */
+TEST(step_CMP_zer_x_lesser)
+{
+    j65c02* inst = nullptr;
+    uint8_t mem[65536];
+    const uint8_t EXPECTED_CMP_INPUT = 0x01;
+    const uint8_t EXPECTED_A_VALUE = 0x05;
+
+    /* clear memory. */
+    memset(mem, 0, sizeof(mem));
+
+    /* set the reset vector. */
+    mem[0xFFFC] = 0x00;
+    mem[0xFFFD] = 0x10;
+
+    /* at 0x1000, add a CMP instruction. */
+    mem[0x1000] = 0xD5;
+    mem[0x1001] = 0x20;
+
+    /* at 0x0025, set the CMP input. */
+    mem[0x0025] = EXPECTED_CMP_INPUT;
+
+    /* create an instance. */
+    TEST_ASSERT(
+        STATUS_SUCCESS
+            == j65c02_create(
+                    &inst, &mem_read, &mem_write, mem,
+                    JEMU_65c02_PERSONALITY_WDC,
+                    JEMU_65c02_EMULATION_MODE_STRICT));
+
+    /* PRECONDITION: crash flag is set. */
+    TEST_EXPECT(j65c02_crash_flag_get(inst));
+
+    /* reset the processor. */
+    TEST_ASSERT(STATUS_SUCCESS == j65c02_reset(inst));
+
+    /* PRECONDITION: the zero flag is set. */
+    j65c02_reg_status_set(
+        inst, j65c02_reg_status_get(inst) | JEMU_65c02_STATUS_ZERO);
+
+    /* PRECONDITION: the negative flag is set. */
+    j65c02_reg_status_set(
+        inst, j65c02_reg_status_get(inst) | JEMU_65c02_STATUS_NEGATIVE);
+
+    /* PRECONDITION: the carry flag is clear. */
+    j65c02_reg_status_set(
+        inst, j65c02_reg_status_get(inst) & ~JEMU_65c02_STATUS_CARRY);
+
+    /* PRECONDITION: set the A register. */
+    j65c02_reg_a_set(inst, EXPECTED_A_VALUE);
+
+    /* PRECONDITION: set the X register. */
+    j65c02_reg_x_set(inst, 0x05);
+
+    /* PRECONDITION: PC is 0x1000. */
+    TEST_ASSERT(0x1000 == j65c02_reg_pc_get(inst));
+
+    /* single step. */
+    TEST_ASSERT(STATUS_SUCCESS == j65c02_step(inst));
+
+    /* POSTCONDITION: PC is 0x1002. */
+    TEST_EXPECT(0x1002 == j65c02_reg_pc_get(inst));
+
+    /* POSTCONDITION: the zero flag is clear. */
+    TEST_EXPECT(!(j65c02_reg_status_get(inst) & JEMU_65c02_STATUS_ZERO));
+
+    /* POSTCONDITION: the negative flag is clear. */
+    TEST_EXPECT(!(j65c02_reg_status_get(inst) & JEMU_65c02_STATUS_NEGATIVE));
+
+    /* POSTCONDITION: the carry flag is set. */
+    TEST_EXPECT(j65c02_reg_status_get(inst) & JEMU_65c02_STATUS_CARRY);
+
+    /* clean up. */
+    TEST_ASSERT(STATUS_SUCCESS == j65c02_release(inst));
+}
+
+/**
+ * 0xD5 CMP ZER X (comparing a greater value).
+ */
+TEST(step_CMP_zer_x_greater)
+{
+    j65c02* inst = nullptr;
+    uint8_t mem[65536];
+    const uint8_t EXPECTED_CMP_INPUT = 0x10;
+    const uint8_t EXPECTED_A_VALUE = 0x05;
+
+    /* clear memory. */
+    memset(mem, 0, sizeof(mem));
+
+    /* set the reset vector. */
+    mem[0xFFFC] = 0x00;
+    mem[0xFFFD] = 0x10;
+
+    /* at 0x1000, add a CMP instruction. */
+    mem[0x1000] = 0xD5;
+    mem[0x1001] = 0x20;
+
+    /* at 0x0025, set the CMP input. */
+    mem[0x0025] = EXPECTED_CMP_INPUT;
+
+    /* create an instance. */
+    TEST_ASSERT(
+        STATUS_SUCCESS
+            == j65c02_create(
+                    &inst, &mem_read, &mem_write, mem,
+                    JEMU_65c02_PERSONALITY_WDC,
+                    JEMU_65c02_EMULATION_MODE_STRICT));
+
+    /* PRECONDITION: crash flag is set. */
+    TEST_EXPECT(j65c02_crash_flag_get(inst));
+
+    /* reset the processor. */
+    TEST_ASSERT(STATUS_SUCCESS == j65c02_reset(inst));
+
+    /* PRECONDITION: the zero flag is set. */
+    j65c02_reg_status_set(
+        inst, j65c02_reg_status_get(inst) | JEMU_65c02_STATUS_ZERO);
+
+    /* PRECONDITION: the negative flag is clear. */
+    j65c02_reg_status_set(
+        inst, j65c02_reg_status_get(inst) & ~JEMU_65c02_STATUS_NEGATIVE);
+
+    /* PRECONDITION: the carry flag is set. */
+    j65c02_reg_status_set(
+        inst, j65c02_reg_status_get(inst) | JEMU_65c02_STATUS_CARRY);
+
+    /* PRECONDITION: set the A register. */
+    j65c02_reg_a_set(inst, EXPECTED_A_VALUE);
+
+    /* PRECONDITION: set the X register. */
+    j65c02_reg_x_set(inst, 0x05);
+
+    /* PRECONDITION: PC is 0x1000. */
+    TEST_ASSERT(0x1000 == j65c02_reg_pc_get(inst));
+
+    /* single step. */
+    TEST_ASSERT(STATUS_SUCCESS == j65c02_step(inst));
+
+    /* POSTCONDITION: PC is 0x1002. */
+    TEST_EXPECT(0x1002 == j65c02_reg_pc_get(inst));
+
+    /* POSTCONDITION: the zero flag is clear. */
+    TEST_EXPECT(!(j65c02_reg_status_get(inst) & JEMU_65c02_STATUS_ZERO));
+
+    /* POSTCONDITION: the negative flag is set. */
+    TEST_EXPECT(j65c02_reg_status_get(inst) & JEMU_65c02_STATUS_NEGATIVE);
+
+    /* POSTCONDITION: the carry flag is clear. */
+    TEST_EXPECT(!(j65c02_reg_status_get(inst) & JEMU_65c02_STATUS_CARRY));
+
+    /* clean up. */
+    TEST_ASSERT(STATUS_SUCCESS == j65c02_release(inst));
+}
