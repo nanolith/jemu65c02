fb2b3ceb450b5479ead63c459eedf9a1112f6498
Justin Handville <nanolith@gmail.com>
2022-12-18 18:24:47 -0500

    impl of the EOR op function.

---
+++ test/test_op_EOR.cpp
@@ -0,0 +1,207 @@
+#include <minunit/minunit.h>
+#include <jemu65c02/jemu65c02.h>
+#include <string.h>
+
+#include "../src/jemu65c02_internal.h"
+
+JEMU_IMPORT_jemu65c02;
+
+TEST_SUITE(j65c02_op_EOR);
+
+static status mem_read(void* varr, uint16_t addr, uint8_t* val)
+{
+    const uint8_t* arr = (const uint8_t*)varr;
+
+    *val = arr[addr];
+
+    return STATUS_SUCCESS;
+}
+
+static status mem_write(void* varr, uint16_t addr, uint8_t val)
+{
+    uint8_t* arr = (uint8_t*)varr;
+
+    arr[addr] = val;
+
+    return STATUS_SUCCESS;
+}
+
+/**
+ * EOR Normal result (not negative or zero).
+ */
+TEST(EOR_basics)
+{
+    j65c02* inst = nullptr;
+    uint8_t mem[65536];
+
+    /* clear memory. */
+    memset(mem, 0, sizeof(mem));
+
+    /* set the reset vector. */
+    mem[0xFFFC] = 0x00;
+    mem[0xFFFD] = 0x10;
+
+    /* create an instance. */
+    TEST_ASSERT(
+        STATUS_SUCCESS
+            == j65c02_create(
+                    &inst, &mem_read, &mem_write, mem,
+                    JEMU_65c02_PERSONALITY_WDC,
+                    JEMU_65c02_EMULATION_MODE_STRICT));
+
+    /* PRECONDITION: crash flag is set. */
+    TEST_EXPECT(j65c02_crash_flag_get(inst));
+
+    /* reset the processor. */
+    TEST_ASSERT(STATUS_SUCCESS == j65c02_reset(inst));
+
+    /* PRECONDITION: A is set to 0x7F. */
+    j65c02_reg_a_set(inst, 0x7F);
+
+    /* PRECONDITION: set the zero flag. */
+    j65c02_reg_status_set(
+        inst, j65c02_reg_status_get(inst) | JEMU_65c02_STATUS_ZERO);
+
+    /* PRECONDITON: set the negative flag. */
+    j65c02_reg_status_set(
+        inst, j65c02_reg_status_get(inst) | JEMU_65c02_STATUS_NEGATIVE);
+
+    /* run the EOR operation. */
+    JEMU_SYM(j65c02_op_EOR)(inst, 0x10);
+
+    /* POSTCONDITION: crash flag is not set. */
+    TEST_EXPECT(!j65c02_crash_flag_get(inst));
+
+    /* POSTCONDITION: A is set to the EOR sum. */
+    TEST_EXPECT(j65c02_reg_a_get(inst) == (0x7F ^ 0x10));
+
+    /* POSTCONDITION: the zero flag is not set. */
+    TEST_EXPECT(0 == (j65c02_reg_status_get(inst) & JEMU_65c02_STATUS_ZERO));
+
+    /* POSTCONDITION: the negative flag is not set. */
+    TEST_EXPECT(
+        0 == (j65c02_reg_status_get(inst) & JEMU_65c02_STATUS_NEGATIVE));
+
+    /* clean up. */
+    TEST_ASSERT(STATUS_SUCCESS == j65c02_release(inst));
+}
+
+/**
+ * EOR Zero result.
+ */
+TEST(EOR_zero)
+{
+    j65c02* inst = nullptr;
+    uint8_t mem[65536];
+
+    /* clear memory. */
+    memset(mem, 0, sizeof(mem));
+
+    /* set the reset vector. */
+    mem[0xFFFC] = 0x00;
+    mem[0xFFFD] = 0x10;
+
+    /* create an instance. */
+    TEST_ASSERT(
+        STATUS_SUCCESS
+            == j65c02_create(
+                    &inst, &mem_read, &mem_write, mem,
+                    JEMU_65c02_PERSONALITY_WDC,
+                    JEMU_65c02_EMULATION_MODE_STRICT));
+
+    /* PRECONDITION: crash flag is set. */
+    TEST_EXPECT(j65c02_crash_flag_get(inst));
+
+    /* reset the processor. */
+    TEST_ASSERT(STATUS_SUCCESS == j65c02_reset(inst));
+
+    /* PRECONDITION: A is set to 0xFF. */
+    j65c02_reg_a_set(inst, 0xFF);
+
+    /* PRECONDITION: clear the zero flag. */
+    j65c02_reg_status_set(
+        inst, j65c02_reg_status_get(inst) & ~JEMU_65c02_STATUS_ZERO);
+
+    /* PRECONDITON: set the negative flag. */
+    j65c02_reg_status_set(
+        inst, j65c02_reg_status_get(inst) | JEMU_65c02_STATUS_NEGATIVE);
+
+    /* run the EOR operation. */
+    JEMU_SYM(j65c02_op_EOR)(inst, 0xFF);
+
+    /* POSTCONDITION: crash flag is not set. */
+    TEST_EXPECT(!j65c02_crash_flag_get(inst));
+
+    /* POSTCONDITION: A is set to the EOR sum. */
+    TEST_EXPECT(j65c02_reg_a_get(inst) == (0xFF ^ 0xFF));
+
+    /* POSTCONDITION: the zero flag is set. */
+    TEST_EXPECT(j65c02_reg_status_get(inst) & JEMU_65c02_STATUS_ZERO);
+
+    /* POSTCONDITION: the negative flag is not set. */
+    TEST_EXPECT(
+        0 == (j65c02_reg_status_get(inst) & JEMU_65c02_STATUS_NEGATIVE));
+
+    /* clean up. */
+    TEST_ASSERT(STATUS_SUCCESS == j65c02_release(inst));
+}
+
+/**
+ * EOR the negative flag is set.
+ */
+TEST(EOR_negative)
+{
+    j65c02* inst = nullptr;
+    uint8_t mem[65536];
+
+    /* clear memory. */
+    memset(mem, 0, sizeof(mem));
+
+    /* set the reset vector. */
+    mem[0xFFFC] = 0x00;
+    mem[0xFFFD] = 0x10;
+
+    /* create an instance. */
+    TEST_ASSERT(
+        STATUS_SUCCESS
+            == j65c02_create(
+                    &inst, &mem_read, &mem_write, mem,
+                    JEMU_65c02_PERSONALITY_WDC,
+                    JEMU_65c02_EMULATION_MODE_STRICT));
+
+    /* PRECONDITION: crash flag is set. */
+    TEST_EXPECT(j65c02_crash_flag_get(inst));
+
+    /* reset the processor. */
+    TEST_ASSERT(STATUS_SUCCESS == j65c02_reset(inst));
+
+    /* PRECONDITION: A is set to 0x80. */
+    j65c02_reg_a_set(inst, 0x80);
+
+    /* PRECONDITION: set the zero flag. */
+    j65c02_reg_status_set(
+        inst, j65c02_reg_status_get(inst) | JEMU_65c02_STATUS_ZERO);
+
+    /* PRECONDITON: clear the negative flag. */
+    j65c02_reg_status_set(
+        inst, j65c02_reg_status_get(inst) & ~JEMU_65c02_STATUS_NEGATIVE);
+
+    /* run the EOR operation. */
+    JEMU_SYM(j65c02_op_EOR)(inst, 0x00);
+
+    /* POSTCONDITION: crash flag is not set. */
+    TEST_EXPECT(!j65c02_crash_flag_get(inst));
+
+    /* POSTCONDITION: A is set to the EOR sum. */
+    TEST_EXPECT(j65c02_reg_a_get(inst) == (0x80 ^ 0x00));
+
+    /* POSTCONDITION: the zero flag is not set. */
+    TEST_EXPECT(0 == (j65c02_reg_status_get(inst) & JEMU_65c02_STATUS_ZERO));
+
+    /* POSTCONDITION: the negative flag is set. */
+    TEST_EXPECT(j65c02_reg_status_get(inst) & JEMU_65c02_STATUS_NEGATIVE);
+
+    /* clean up. */
+    TEST_ASSERT(STATUS_SUCCESS == j65c02_release(inst));
+}
+
