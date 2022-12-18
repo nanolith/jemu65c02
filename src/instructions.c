f36ed51512afea3f294060a804cbacca5ced98f3
Justin Handville <nanolith@gmail.com>
2022-12-18 01:04:58 -0500

    impl of DEC abs x instruction.

--- src/instructions.c
+++ src/instructions.c
@@ -486,8 +486,8 @@ JEMU_SYM(j65c02_instruction) JEMU_SYM(global_j65c02_instructions)[256] = {
     { .exec = &JEMU_SYM(j65c02_inst_invalid_opcode), .max_cycles = 2 },
     /* Opcode 0xDD - CMP ABS X instruction. */
     { .exec = &JEMU_SYM(j65c02_inst_CMP_abs_x), .max_cycles = 4 },
-    /* Opcode 0xDE - TODO implement. */
-    { .exec = &JEMU_SYM(j65c02_inst_invalid_opcode), .max_cycles = 2 },
+    /* Opcode 0xDE - DEC ABS X instruction. */
+    { .exec = &JEMU_SYM(j65c02_inst_DEC_abs_x), .max_cycles = 7 },
     /* Opcode 0xDF - TODO implement. */
     { .exec = &JEMU_SYM(j65c02_inst_invalid_opcode), .max_cycles = 2 },
 
