12782b0e6e1d5c2ade62bf0aff68560ee2e47949
Justin Handville <nanolith@gmail.com>
2022-12-18 02:38:17 -0500

    impl of the DEX instruction.

---
+++ test/test_DEX.cpp
@@ -0,0 +1,220 @@
+#include <minunit/minunit.h>
+#include <jemu65c02/jemu65c02.h>
+#include <string.h>
+
+JEMU_IMPORT_jemu65c02;
+
+TEST_SUITE(j65c02_DEX);
+
+static status mem_read(void* varr, uint16_t addr, uint8_t* val)
+{
+    const uint8_t* arr = (const uint8_t*)varr;
+
+    *val = arr[addr];
+
+    return STATUS_SUCCESS;
+}
+
+static status mem_write(void* varr, uint16_t addr, uint8_t val)
+{
+    uint8_t* arr = (uint8_t*)varr;
+
+    arr[addr] = val;
+
+    return STATUS_SUCCESS;
+}
+
+/**
+ * 0xCA DEX (non-zero / non-negative to non-zero / non-negative).
+ */
+TEST(step_DEX_non_zero_non_negative)
+{
+    j65c02* inst = nullptr;
+    uint8_t mem[65536];
+
+    /* clear memory. */
+    memset(mem, 0, sizeof(mem));
+
+    /* set the reset vector. */
+    mem[0xFFFC] = 0x00;
+    mem[0xFFFD] = 0x10;
+
+    /* at 0x1000, add a DEX instruction. */
+    mem[0x1000] = 0xCA;
+
+    /* create an instance. */
+    TEST_ASSERT(
+        STATUS_SUCCESS
+            == j65c02_create(
+                    &inst, &mem_read, &mem_write, mem,
+                    JEMU_65c02_PERSONALITY_WDC,
+                    JEMU_65c02_EMULATION_MODE_STRICT));
+
+    /* PRECONDITION: crash flag is set. */
+    TEST_EXPECT(j65c02_crash_flag_get(inst));
+
+    /* reset the processor. */
+    TEST_ASSERT(STATUS_SUCCESS == j65c02_reset(inst));
+
+    /* PRECONDITION: the zero flag is set. */
+    j65c02_reg_status_set(
+        inst, j65c02_reg_status_get(inst) | JEMU_65c02_STATUS_ZERO);
+
+    /* PRECONDITION: the negative flag is set. */
+    j65c02_reg_status_set(
+        inst, j65c02_reg_status_get(inst) | JEMU_65c02_STATUS_NEGATIVE);
+
+    /* PRECONDITION: set the X register. */
+    j65c02_reg_x_set(inst, 0x02);
+
+    /* PRECONDITION: PC is 0x1000. */
+    TEST_ASSERT(0x1000 == j65c02_reg_pc_get(inst));
+
+    /* single step. */
+    TEST_ASSERT(STATUS_SUCCESS == j65c02_step(inst));
+
+    /* POSTCONDITION: PC is 0x1001. */
+    TEST_EXPECT(0x1001 == j65c02_reg_pc_get(inst));
+
+    /* POSTCONDITION: X is 0x01. */
+    TEST_EXPECT(0x01 == j65c02_reg_x_get(inst));
+
+    /* POSTCONDITION: the zero flag is clear. */
+    TEST_EXPECT(!(j65c02_reg_status_get(inst) & JEMU_65c02_STATUS_ZERO));
+
+    /* POSTCONDITION: the negative flag is clear. */
+    TEST_EXPECT(!(j65c02_reg_status_get(inst) & JEMU_65c02_STATUS_NEGATIVE));
+
+    /* clean up. */
+    TEST_ASSERT(STATUS_SUCCESS == j65c02_release(inst));
+}
+
+/**
+ * 0xCA DEX (0x01 to zero).
+ */
+TEST(step_DEX_zero)
+{
+    j65c02* inst = nullptr;
+    uint8_t mem[65536];
+
+    /* clear memory. */
+    memset(mem, 0, sizeof(mem));
+
+    /* set the reset vector. */
+    mem[0xFFFC] = 0x00;
+    mem[0xFFFD] = 0x10;
+
+    /* at 0x1000, add a DEX instruction. */
+    mem[0x1000] = 0xCA;
+
+    /* create an instance. */
+    TEST_ASSERT(
+        STATUS_SUCCESS
+            == j65c02_create(
+                    &inst, &mem_read, &mem_write, mem,
+                    JEMU_65c02_PERSONALITY_WDC,
+                    JEMU_65c02_EMULATION_MODE_STRICT));
+
+    /* PRECONDITION: crash flag is set. */
+    TEST_EXPECT(j65c02_crash_flag_get(inst));
+
+    /* reset the processor. */
+    TEST_ASSERT(STATUS_SUCCESS == j65c02_reset(inst));
+
+    /* PRECONDITION: the zero flag is clear. */
+    j65c02_reg_status_set(
+        inst, j65c02_reg_status_get(inst) & ~JEMU_65c02_STATUS_ZERO);
+
+    /* PRECONDITION: the negative flag is set. */
+    j65c02_reg_status_set(
+        inst, j65c02_reg_status_get(inst) | JEMU_65c02_STATUS_NEGATIVE);
+
+    /* PRECONDITION: set the X register. */
+    j65c02_reg_x_set(inst, 0x01);
+
+    /* PRECONDITION: PC is 0x1000. */
+    TEST_ASSERT(0x1000 == j65c02_reg_pc_get(inst));
+
+    /* single step. */
+    TEST_ASSERT(STATUS_SUCCESS == j65c02_step(inst));
+
+    /* POSTCONDITION: PC is 0x1001. */
+    TEST_EXPECT(0x1001 == j65c02_reg_pc_get(inst));
+
+    /* POSTCONDITION: X is 0x00. */
+    TEST_EXPECT(0x00 == j65c02_reg_x_get(inst));
+
+    /* POSTCONDITION: the zero flag is set. */
+    TEST_EXPECT(j65c02_reg_status_get(inst) & JEMU_65c02_STATUS_ZERO);
+
+    /* POSTCONDITION: the negative flag is clear. */
+    TEST_EXPECT(!(j65c02_reg_status_get(inst) & JEMU_65c02_STATUS_NEGATIVE));
+
+    /* clean up. */
+    TEST_ASSERT(STATUS_SUCCESS == j65c02_release(inst));
+}
+
+/**
+ * 0xCA DEX (0x00 to 0xFF).
+ */
+TEST(step_DEX_negative)
+{
+    j65c02* inst = nullptr;
+    uint8_t mem[65536];
+
+    /* clear memory. */
+    memset(mem, 0, sizeof(mem));
+
+    /* set the reset vector. */
+    mem[0xFFFC] = 0x00;
+    mem[0xFFFD] = 0x10;
+
+    /* at 0x1000, add a DEX instruction. */
+    mem[0x1000] = 0xCA;
+
+    /* create an instance. */
+    TEST_ASSERT(
+        STATUS_SUCCESS
+            == j65c02_create(
+                    &inst, &mem_read, &mem_write, mem,
+                    JEMU_65c02_PERSONALITY_WDC,
+                    JEMU_65c02_EMULATION_MODE_STRICT));
+
+    /* PRECONDITION: crash flag is set. */
+    TEST_EXPECT(j65c02_crash_flag_get(inst));
+
+    /* reset the processor. */
+    TEST_ASSERT(STATUS_SUCCESS == j65c02_reset(inst));
+
+    /* PRECONDITION: the zero flag is set. */
+    j65c02_reg_status_set(
+        inst, j65c02_reg_status_get(inst) | JEMU_65c02_STATUS_ZERO);
+
+    /* PRECONDITION: the negative flag is clear. */
+    j65c02_reg_status_set(
+        inst, j65c02_reg_status_get(inst) & ~JEMU_65c02_STATUS_NEGATIVE);
+
+    /* PRECONDITION: set the X register. */
+    j65c02_reg_x_set(inst, 0x00);
+
+    /* PRECONDITION: PC is 0x1000. */
+    TEST_ASSERT(0x1000 == j65c02_reg_pc_get(inst));
+
+    /* single step. */
+    TEST_ASSERT(STATUS_SUCCESS == j65c02_step(inst));
+
+    /* POSTCONDITION: PC is 0x1001. */
+    TEST_EXPECT(0x1001 == j65c02_reg_pc_get(inst));
+
+    /* POSTCONDITION: X is 0xFF. */
+    TEST_EXPECT(0xFF == j65c02_reg_x_get(inst));
+
+    /* POSTCONDITION: the zero flag is clear. */
+    TEST_EXPECT(!(j65c02_reg_status_get(inst) & JEMU_65c02_STATUS_ZERO));
+
+    /* POSTCONDITION: the negative flag is set. */
+    TEST_EXPECT(j65c02_reg_status_get(inst) & JEMU_65c02_STATUS_NEGATIVE);
+
+    /* clean up. */
+    TEST_ASSERT(STATUS_SUCCESS == j65c02_release(inst));
+}
