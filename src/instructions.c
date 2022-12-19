a62b9ae6cf42eca60296f20ba4a13cee325429f9
Justin Handville <nanolith@gmail.com>
2022-12-19 11:16:37 -0500

    impl of EOR zer x instruction.

--- src/instructions.c
+++ src/instructions.c
@@ -196,8 +196,8 @@ JEMU_SYM(j65c02_instruction) JEMU_SYM(global_j65c02_instructions)[256] = {
     { .exec = &JEMU_SYM(j65c02_inst_invalid_opcode), .max_cycles = 2 },
     /* Opcode 0x54 - TODO implement. */
     { .exec = &JEMU_SYM(j65c02_inst_invalid_opcode), .max_cycles = 2 },
-    /* Opcode 0x55 - TODO implement. */
-    { .exec = &JEMU_SYM(j65c02_inst_invalid_opcode), .max_cycles = 2 },
+    /* Opcode 0x55 - EOR ZER X instruction. */
+    { .exec = &JEMU_SYM(j65c02_inst_EOR_zer_x), .max_cycles = 4 },
     /* Opcode 0x56 - TODO implement. */
     { .exec = &JEMU_SYM(j65c02_inst_invalid_opcode), .max_cycles = 2 },
     /* Opcode 0x57 - TODO implement. */
