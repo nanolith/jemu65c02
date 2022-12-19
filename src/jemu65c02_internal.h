37f324f93ae3d0be189833c9488c0cb5bdcbca4a
Justin Handville <nanolith@gmail.com>
2022-12-19 14:55:27 -0500

    impl of INY instruction.

--- src/jemu65c02_internal.h
+++ src/jemu65c02_internal.h
@@ -1362,6 +1362,21 @@ JEMU_SYM(status) JEMU_SYM(j65c02_inst_INC_zer_x)(
 JEMU_SYM(status) JEMU_SYM(j65c02_inst_INX)(
     JEMU_SYM(j65c02)* inst, int* cycles);
 
+/**
+ * \brief Handle an INY instruction.
+ *
+ * \param inst              The emulator instance on which this instruction
+ *                          executes.
+ * \param cycles            The number of cycles taken to execute this
+ *                          instruction.
+ *
+ * \returns a status code indicating success or failure.
+ *      - STATUS_SUCCESS on success.
+ *      - a non-zero error code on failure.
+ */
+JEMU_SYM(status) JEMU_SYM(j65c02_inst_INY)(
+    JEMU_SYM(j65c02)* inst, int* cycles);
+
 /******************************************************************************/
 /* Start of public exports.                                                   */
 /******************************************************************************/
