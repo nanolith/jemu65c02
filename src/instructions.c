710bd8f2e053295d6537e3d89a337f38789be8da
Justin Handville <nanolith@gmail.com>
2022-12-18 23:00:20 -0500

    impl of EOR abs x instruction.

--- src/instructions.c
+++ src/instructions.c
@@ -212,8 +212,8 @@ JEMU_SYM(j65c02_instruction) JEMU_SYM(global_j65c02_instructions)[256] = {
     { .exec = &JEMU_SYM(j65c02_inst_invalid_opcode), .max_cycles = 2 },
     /* Opcode 0x5C - TODO implement. */
     { .exec = &JEMU_SYM(j65c02_inst_invalid_opcode), .max_cycles = 2 },
-    /* Opcode 0x5D - TODO implement. */
-    { .exec = &JEMU_SYM(j65c02_inst_invalid_opcode), .max_cycles = 2 },
+    /* Opcode 0x5D - EOR ABS X instruction. */
+    { .exec = &JEMU_SYM(j65c02_inst_EOR_abs_x), .max_cycles = 4 },
     /* Opcode 0x5E - TODO implement. */
     { .exec = &JEMU_SYM(j65c02_inst_invalid_opcode), .max_cycles = 2 },
     /* Opcode 0x5F - TODO implement. */
