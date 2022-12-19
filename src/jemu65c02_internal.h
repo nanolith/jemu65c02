d797a0300148a15c7b1f87f1781154bd46e1ec98
Justin Handville <nanolith@gmail.com>
2022-12-19 13:56:06 -0500

    impl of INC abs instruction.

--- src/jemu65c02_internal.h
+++ src/jemu65c02_internal.h
@@ -1287,6 +1287,21 @@ JEMU_SYM(status) JEMU_SYM(j65c02_inst_EOR_zer_y_idr)(
 JEMU_SYM(status) JEMU_SYM(j65c02_inst_INC_a)(
     JEMU_SYM(j65c02)* inst, int* cycles);
 
+/**
+ * \brief Handle an INC ABS instruction.
+ *
+ * \param inst              The emulator instance on which this instruction
+ *                          executes.
+ * \param cycles            The number of cycles taken to execute this
+ *                          instruction.
+ *
+ * \returns a status code indicating success or failure.
+ *      - STATUS_SUCCESS on success.
+ *      - a non-zero error code on failure.
+ */
+JEMU_SYM(status) JEMU_SYM(j65c02_inst_INC_abs)(
+    JEMU_SYM(j65c02)* inst, int* cycles);
+
 /******************************************************************************/
 /* Start of public exports.                                                   */
 /******************************************************************************/
