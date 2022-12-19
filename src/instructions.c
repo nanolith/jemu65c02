660a1de7294f88ef09cbe1579528bb8f1f7152d0
Justin Handville <nanolith@gmail.com>
2022-12-19 14:33:50 -0500

    impl of INC zer x instruction.

--- src/instructions.c
+++ src/instructions.c
@@ -538,8 +538,8 @@ JEMU_SYM(j65c02_instruction) JEMU_SYM(global_j65c02_instructions)[256] = {
     { .exec = &JEMU_SYM(j65c02_inst_invalid_opcode), .max_cycles = 2 },
     /* Opcode 0xF5 - TODO implement. */
     { .exec = &JEMU_SYM(j65c02_inst_invalid_opcode), .max_cycles = 2 },
-    /* Opcode 0xF6 - TODO implement. */
-    { .exec = &JEMU_SYM(j65c02_inst_invalid_opcode), .max_cycles = 2 },
+    /* Opcode 0xF6 - INC ZER X instruction. */
+    { .exec = &JEMU_SYM(j65c02_inst_INC_zer_x), .max_cycles = 6 },
     /* Opcode 0xF7 - TODO implement. */
     { .exec = &JEMU_SYM(j65c02_inst_invalid_opcode), .max_cycles = 2 },
     /* Opcode 0xF8 - TODO implement. */
