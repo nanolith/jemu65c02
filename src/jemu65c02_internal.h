f038ef945dd6a4137c4b879be7cc09adb464254f
Justin Handville <nanolith@gmail.com>
2022-12-20 12:20:05 -0500

    impl of LDX abs y instruction.

--- src/jemu65c02_internal.h
+++ src/jemu65c02_internal.h
@@ -1637,6 +1637,21 @@ JEMU_SYM(status) JEMU_SYM(j65c02_inst_LDX_abs)(
 JEMU_SYM(status) JEMU_SYM(j65c02_inst_LDX_zer)(
     JEMU_SYM(j65c02)* inst, int* cycles);
 
+/**
+ * \brief Handle an LDX ABS Y instruction.
+ *
+ * \param inst              The emulator instance on which this instruction
+ *                          executes.
+ * \param cycles            The number of cycles taken to execute this
+ *                          instruction.
+ *
+ * \returns a status code indicating success or failure.
+ *      - STATUS_SUCCESS on success.
+ *      - a non-zero error code on failure.
+ */
+JEMU_SYM(status) JEMU_SYM(j65c02_inst_LDX_abs_y)(
+    JEMU_SYM(j65c02)* inst, int* cycles);
+
 /******************************************************************************/
 /* Start of public exports.                                                   */
 /******************************************************************************/
