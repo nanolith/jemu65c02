c14edda9254125265960ad9776c67c0f686cb759
Justin Handville <nanolith@gmail.com>
2022-12-20 15:14:25 -0500

    impl of LDY zer x instruction.

--- src/instructions.c
+++ src/instructions.c
@@ -398,8 +398,8 @@ JEMU_SYM(j65c02_instruction) JEMU_SYM(global_j65c02_instructions)[256] = {
     { .exec = &JEMU_SYM(j65c02_inst_LDA_zer_idr), .max_cycles = 5 },
     /* Opcode 0xB3 - TODO implement. */
     { .exec = &JEMU_SYM(j65c02_inst_invalid_opcode), .max_cycles = 2 },
-    /* Opcode 0xB4 - TODO implement. */
-    { .exec = &JEMU_SYM(j65c02_inst_invalid_opcode), .max_cycles = 2 },
+    /* Opcode 0xB4 - LDY ZER X. */
+    { .exec = &JEMU_SYM(j65c02_inst_LDY_zer_x), .max_cycles = 4 },
     /* Opcode 0xB5 - LDA ZER X. */
     { .exec = &JEMU_SYM(j65c02_inst_LDA_zer_x), .max_cycles = 4 },
     /* Opcode 0xB6 - LDX ZER Y. */
