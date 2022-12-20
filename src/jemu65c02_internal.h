d6071c90e6c6d09eec0750875e70f3c29d81ab58
Justin Handville <nanolith@gmail.com>
2022-12-20 14:52:50 -0500

    impl of LDY abs instruction.

--- src/jemu65c02_internal.h
+++ src/jemu65c02_internal.h
@@ -1692,6 +1692,21 @@ void JEMU_SYM(j65c02_op_LDY)(
 JEMU_SYM(status) JEMU_SYM(j65c02_inst_LDY_imm)(
     JEMU_SYM(j65c02)* inst, int* cycles);
 
+/**
+ * \brief Handle an LDY ABS instruction.
+ *
+ * \param inst              The emulator instance on which this instruction
+ *                          executes.
+ * \param cycles            The number of cycles taken to execute this
+ *                          instruction.
+ *
+ * \returns a status code indicating success or failure.
+ *      - STATUS_SUCCESS on success.
+ *      - a non-zero error code on failure.
+ */
+JEMU_SYM(status) JEMU_SYM(j65c02_inst_LDY_abs)(
+    JEMU_SYM(j65c02)* inst, int* cycles);
+
 /******************************************************************************/
 /* Start of public exports.                                                   */
 /******************************************************************************/
