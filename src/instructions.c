18d46ecab93a7b98d67b80bc00901f68b214c62c
Justin Handville <nanolith@gmail.com>
2022-12-20 14:45:44 -0500

    impl of LDY imm instruction.

--- src/instructions.c
+++ src/instructions.c
@@ -356,8 +356,8 @@ JEMU_SYM(j65c02_instruction) JEMU_SYM(global_j65c02_instructions)[256] = {
     { .exec = &JEMU_SYM(j65c02_inst_invalid_opcode), .max_cycles = 2 },
 
     /* opcodes 0xA0 - 0xAF. */
-    /* Opcode 0xA0 - TODO implement. */
-    { .exec = &JEMU_SYM(j65c02_inst_invalid_opcode), .max_cycles = 2 },
+    /* Opcode 0xA0 - LDY IMM instruction. */
+    { .exec = &JEMU_SYM(j65c02_inst_LDY_imm), .max_cycles = 2 },
     /* Opcode 0xA1 - LDA ZER X IDR instruction. */
     { .exec = &JEMU_SYM(j65c02_inst_LDA_zer_x_idr), .max_cycles = 6 },
     /* Opcode 0xA2 - LDX IMM instruction. */
