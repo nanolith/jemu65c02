2f1b9d62f781741b60e2a4bb81d0cec3da29d5ca
Justin Handville <nanolith@gmail.com>
2022-12-19 23:46:19 -0500

    impl of LDA abs instruction.

--- src/instructions.c
+++ src/instructions.c
@@ -382,8 +382,8 @@ JEMU_SYM(j65c02_instruction) JEMU_SYM(global_j65c02_instructions)[256] = {
     { .exec = &JEMU_SYM(j65c02_inst_invalid_opcode), .max_cycles = 2 },
     /* Opcode 0xAC - TODO implement. */
     { .exec = &JEMU_SYM(j65c02_inst_invalid_opcode), .max_cycles = 2 },
-    /* Opcode 0xAD - TODO implement. */
-    { .exec = &JEMU_SYM(j65c02_inst_invalid_opcode), .max_cycles = 2 },
+    /* Opcode 0xAD - LDA ABS instruction. */
+    { .exec = &JEMU_SYM(j65c02_inst_LDA_abs), .max_cycles = 4 },
     /* Opcode 0xAE - TODO implement. */
     { .exec = &JEMU_SYM(j65c02_inst_invalid_opcode), .max_cycles = 2 },
     /* Opcode 0xAF - TODO implement. */
