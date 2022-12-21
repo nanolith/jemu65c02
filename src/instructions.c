2c8198bb0f26fd4102e891db64a3b0702b0610f1
Justin Handville <nanolith@gmail.com>
2022-12-21 14:46:42 -0500

    impl of ORA zer instruction.

--- src/instructions.c
+++ src/instructions.c
@@ -26,8 +26,8 @@ JEMU_SYM(j65c02_instruction) JEMU_SYM(global_j65c02_instructions)[256] = {
     { .exec = &JEMU_SYM(j65c02_inst_invalid_opcode), .max_cycles = 2 },
     /* Opcode 0x04 - TODO implement. */
     { .exec = &JEMU_SYM(j65c02_inst_invalid_opcode), .max_cycles = 2 },
-    /* Opcode 0x05 - TODO implement. */
-    { .exec = &JEMU_SYM(j65c02_inst_invalid_opcode), .max_cycles = 2 },
+    /* Opcode 0x05 - ORA ZER instruction. */
+    { .exec = &JEMU_SYM(j65c02_inst_ORA_zer), .max_cycles = 3 },
     /* Opcode 0x06 - ASL ZER instruction. */
     { .exec = &JEMU_SYM(j65c02_inst_ASL_zer), .max_cycles = 5 },
     /* Opcode 0x07 - TODO implement. */
