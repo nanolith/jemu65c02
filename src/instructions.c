092a52dc2ffb3d880da33d3eefb2a58f29145ae8
Justin Handville <nanolith@gmail.com>
2022-12-21 14:40:30 -0500

    impl of the ORA abs instruction.

--- src/instructions.c
+++ src/instructions.c
@@ -42,8 +42,8 @@ JEMU_SYM(j65c02_instruction) JEMU_SYM(global_j65c02_instructions)[256] = {
     { .exec = &JEMU_SYM(j65c02_inst_invalid_opcode), .max_cycles = 2 },
     /* Opcode 0x0C - TODO implement. */
     { .exec = &JEMU_SYM(j65c02_inst_invalid_opcode), .max_cycles = 2 },
-    /* Opcode 0x0D - TODO implement. */
-    { .exec = &JEMU_SYM(j65c02_inst_invalid_opcode), .max_cycles = 2 },
+    /* Opcode 0x0D - ORA ABS instruction. */
+    { .exec = &JEMU_SYM(j65c02_inst_ORA_abs), .max_cycles = 4 },
     /* Opcode 0x0E - ASL abs instruction. */
     { .exec = &JEMU_SYM(j65c02_inst_ASL_abs), .max_cycles = 6 },
     /* Opcode 0x0F - TODO implement. */
