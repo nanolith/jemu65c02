79dece3dc7707fdc43c8154ff378e6cb1eb25f07
Justin Handville <nanolith@gmail.com>
2022-12-21 15:10:49 -0500

    impl of the ORA abs y instruction.

---
+++ test/test_ORA_abs_y.cpp
@@ -0,0 +1,264 @@
+#include <minunit/minunit.h>
+#include <jemu65c02/jemu65c02.h>
+#include <string.h>
+
+JEMU_IMPORT_jemu65c02;
+
+TEST_SUITE(j65c02_ORA_abs_y);
+
+static status mem_read(void* varr, uint16_t addr, uint8_t* val)
+{
+    const uint8_t* arr = (const uint8_t*)varr;
+
+    *val = arr[addr];
+
+    return STATUS_SUCCESS;
+}
+
+static status mem_write(void* varr, uint16_t addr, uint8_t val)
+{
+    uint8_t* arr = (uint8_t*)varr;
+
+    arr[addr] = val;
+
+    return STATUS_SUCCESS;
+}
+
+/**
+ * 0x19 ORA ABS Y normal (non-zero / non-negative).
+ */
+TEST(step_ORA_abs_y_basics)
+{
+    j65c02* inst = nullptr;
+    uint8_t mem[65536];
+    const uint8_t EXPECTED_ORA_INPUT = 0x11;
+    const uint8_t EXPECTED_A_VALUE = 0x22;
+    const uint8_t EXPECTED_OUTPUT = EXPECTED_ORA_INPUT | EXPECTED_A_VALUE;
+
+    /* clear memory. */
+    memset(mem, 0, sizeof(mem));
+
+    /* set the reset vector. */
+    mem[0xFFFC] = 0x00;
+    mem[0xFFFD] = 0x10;
+
+    /* at 0x1000, add an ORA instruction. */
+    mem[0x1000] = 0x19;
+    mem[0x1001] = 0x00;
+    mem[0x1002] = 0x20;
+
+    /* at 0x2005, add the ORA input. */
+    mem[0x2005] = EXPECTED_ORA_INPUT;
+
+    /* create an instance. */
+    TEST_ASSERT(
+        STATUS_SUCCESS
+            == j65c02_create(
+                    &inst, &mem_read, &mem_write, mem,
+                    JEMU_65c02_PERSONALITY_WDC,
+                    JEMU_65c02_EMULATION_MODE_STRICT));
+
+    /* PRECONDITION: crash flag is set. */
+    TEST_EXPECT(j65c02_crash_flag_get(inst));
+
+    /* reset the processor. */
+    TEST_ASSERT(STATUS_SUCCESS == j65c02_reset(inst));
+
+    /* PRECONDITION: A is set to EXPECTED_A_VALUE. */
+    j65c02_reg_a_set(inst, EXPECTED_A_VALUE);
+
+    /* PRECONDITION: Y is set to 0x05. */
+    j65c02_reg_y_set(inst, 0x05);
+
+    /* PRECONDITION: set the zero flag. */
+    j65c02_reg_status_set(
+        inst, j65c02_reg_status_get(inst) | JEMU_65c02_STATUS_ZERO);
+
+    /* PRECONDITON: set the negative flag. */
+    j65c02_reg_status_set(
+        inst, j65c02_reg_status_get(inst) | JEMU_65c02_STATUS_NEGATIVE);
+
+    /* PRECONDITION: PC is 0x1000. */
+    TEST_ASSERT(0x1000 == j65c02_reg_pc_get(inst));
+
+    /* single step. */
+    TEST_ASSERT(STATUS_SUCCESS == j65c02_step(inst));
+
+    /* POSTCONDITION: PC is 0x1003. */
+    TEST_EXPECT(0x1003 == j65c02_reg_pc_get(inst));
+
+    /* POSTCONDITION: crash flag is not set. */
+    TEST_EXPECT(!j65c02_crash_flag_get(inst));
+
+    /* POSTCONDITION: A is set to the output. */
+    TEST_EXPECT(j65c02_reg_a_get(inst) == EXPECTED_OUTPUT);
+
+    /* POSTCONDITION: the zero flag is not set. */
+    TEST_EXPECT(0 == (j65c02_reg_status_get(inst) & JEMU_65c02_STATUS_ZERO));
+
+    /* POSTCONDITION: the negative flag is not set. */
+    TEST_EXPECT(
+        0 == (j65c02_reg_status_get(inst) & JEMU_65c02_STATUS_NEGATIVE));
+
+    /* clean up. */
+    TEST_ASSERT(STATUS_SUCCESS == j65c02_release(inst));
+}
+
+/**
+ * 0x19 ORA ABS Y zero.
+ */
+TEST(step_ORA_abs_y_zero)
+{
+    j65c02* inst = nullptr;
+    uint8_t mem[65536];
+    const uint8_t EXPECTED_ORA_INPUT = 0x00;
+    const uint8_t EXPECTED_A_VALUE = 0x00;
+    const uint8_t EXPECTED_OUTPUT = EXPECTED_ORA_INPUT | EXPECTED_A_VALUE;
+
+    /* clear memory. */
+    memset(mem, 0, sizeof(mem));
+
+    /* set the reset vector. */
+    mem[0xFFFC] = 0x00;
+    mem[0xFFFD] = 0x10;
+
+    /* at 0x1000, add an ORA instruction. */
+    mem[0x1000] = 0x19;
+    mem[0x1001] = 0x00;
+    mem[0x1002] = 0x20;
+
+    /* at 0x2005, add the ORA input. */
+    mem[0x2005] = EXPECTED_ORA_INPUT;
+
+    /* create an instance. */
+    TEST_ASSERT(
+        STATUS_SUCCESS
+            == j65c02_create(
+                    &inst, &mem_read, &mem_write, mem,
+                    JEMU_65c02_PERSONALITY_WDC,
+                    JEMU_65c02_EMULATION_MODE_STRICT));
+
+    /* PRECONDITION: crash flag is set. */
+    TEST_EXPECT(j65c02_crash_flag_get(inst));
+
+    /* reset the processor. */
+    TEST_ASSERT(STATUS_SUCCESS == j65c02_reset(inst));
+
+    /* PRECONDITION: A is set to EXPECTED_A_VALUE. */
+    j65c02_reg_a_set(inst, EXPECTED_A_VALUE);
+
+    /* PRECONDITION: Y is set to 0x05. */
+    j65c02_reg_y_set(inst, 0x05);
+
+    /* PRECONDITION: clear the zero flag. */
+    j65c02_reg_status_set(
+        inst, j65c02_reg_status_get(inst) & ~JEMU_65c02_STATUS_ZERO);
+
+    /* PRECONDITON: set the negative flag. */
+    j65c02_reg_status_set(
+        inst, j65c02_reg_status_get(inst) | JEMU_65c02_STATUS_NEGATIVE);
+
+    /* PRECONDITION: PC is 0x1000. */
+    TEST_ASSERT(0x1000 == j65c02_reg_pc_get(inst));
+
+    /* single step. */
+    TEST_ASSERT(STATUS_SUCCESS == j65c02_step(inst));
+
+    /* POSTCONDITION: PC is 0x1003. */
+    TEST_EXPECT(0x1003 == j65c02_reg_pc_get(inst));
+
+    /* POSTCONDITION: crash flag is not set. */
+    TEST_EXPECT(!j65c02_crash_flag_get(inst));
+
+    /* POSTCONDITION: A is set to the output. */
+    TEST_EXPECT(j65c02_reg_a_get(inst) == EXPECTED_OUTPUT);
+
+    /* POSTCONDITION: the zero flag is set. */
+    TEST_EXPECT(j65c02_reg_status_get(inst) & JEMU_65c02_STATUS_ZERO);
+
+    /* POSTCONDITION: the negative flag is not set. */
+    TEST_EXPECT(
+        0 == (j65c02_reg_status_get(inst) & JEMU_65c02_STATUS_NEGATIVE));
+
+    /* clean up. */
+    TEST_ASSERT(STATUS_SUCCESS == j65c02_release(inst));
+}
+
+/**
+ * 0x19 ORA ABS Y negative.
+ */
+TEST(step_ORA_abs_y_negative)
+{
+    j65c02* inst = nullptr;
+    uint8_t mem[65536];
+    const uint8_t EXPECTED_ORA_INPUT = 0x01;
+    const uint8_t EXPECTED_A_VALUE = 0x82;
+    const uint8_t EXPECTED_OUTPUT = EXPECTED_ORA_INPUT | EXPECTED_A_VALUE;
+
+    /* clear memory. */
+    memset(mem, 0, sizeof(mem));
+
+    /* set the reset vector. */
+    mem[0xFFFC] = 0x00;
+    mem[0xFFFD] = 0x10;
+
+    /* at 0x1000, add an ORA instruction. */
+    mem[0x1000] = 0x19;
+    mem[0x1001] = 0x00;
+    mem[0x1002] = 0x20;
+
+    /* at 0x2005, add the ORA input. */
+    mem[0x2005] = EXPECTED_ORA_INPUT;
+
+    /* create an instance. */
+    TEST_ASSERT(
+        STATUS_SUCCESS
+            == j65c02_create(
+                    &inst, &mem_read, &mem_write, mem,
+                    JEMU_65c02_PERSONALITY_WDC,
+                    JEMU_65c02_EMULATION_MODE_STRICT));
+
+    /* PRECONDITION: crash flag is set. */
+    TEST_EXPECT(j65c02_crash_flag_get(inst));
+
+    /* reset the processor. */
+    TEST_ASSERT(STATUS_SUCCESS == j65c02_reset(inst));
+
+    /* PRECONDITION: A is set to EXPECTED_A_VALUE. */
+    j65c02_reg_a_set(inst, EXPECTED_A_VALUE);
+
+    /* PRECONDITION: Y is set to 0x05. */
+    j65c02_reg_y_set(inst, 0x05);
+
+    /* PRECONDITION: set the zero flag. */
+    j65c02_reg_status_set(
+        inst, j65c02_reg_status_get(inst) | JEMU_65c02_STATUS_ZERO);
+
+    /* PRECONDITON: clear the negative flag. */
+    j65c02_reg_status_set(
+        inst, j65c02_reg_status_get(inst) & ~JEMU_65c02_STATUS_NEGATIVE);
+
+    /* PRECONDITION: PC is 0x1000. */
+    TEST_ASSERT(0x1000 == j65c02_reg_pc_get(inst));
+
+    /* single step. */
+    TEST_ASSERT(STATUS_SUCCESS == j65c02_step(inst));
+
+    /* POSTCONDITION: PC is 0x1003. */
+    TEST_EXPECT(0x1003 == j65c02_reg_pc_get(inst));
+
+    /* POSTCONDITION: crash flag is not set. */
+    TEST_EXPECT(!j65c02_crash_flag_get(inst));
+
+    /* POSTCONDITION: A is set to the output. */
+    TEST_EXPECT(j65c02_reg_a_get(inst) == EXPECTED_OUTPUT);
+
+    /* POSTCONDITION: the zero flag is not set. */
+    TEST_EXPECT(0 == (j65c02_reg_status_get(inst) & JEMU_65c02_STATUS_ZERO));
+
+    /* POSTCONDITION: the negative flag is set. */
+    TEST_EXPECT(j65c02_reg_status_get(inst) & JEMU_65c02_STATUS_NEGATIVE);
+
+    /* clean up. */
+    TEST_ASSERT(STATUS_SUCCESS == j65c02_release(inst));
+}
