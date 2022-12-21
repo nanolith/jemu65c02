f7ecbc264535fcf7361ec67bfc6bda5609e1cef9
Justin Handville <nanolith@gmail.com>
2022-12-21 14:32:50 -0500

    impl of the ORA imm instruction.

--- src/jemu65c02_internal.h
+++ src/jemu65c02_internal.h
@@ -1847,6 +1847,21 @@ JEMU_SYM(status) JEMU_SYM(j65c02_inst_LSR_zer_x)(
 void JEMU_SYM(j65c02_op_ORA)(
     JEMU_SYM(j65c02)* inst, uint8_t val);
 
+/**
+ * \brief Handle an ORA IMM instruction.
+ *
+ * \param inst              The emulator instance on which this instruction
+ *                          executes.
+ * \param cycles            The number of cycles taken to execute this
+ *                          instruction.
+ *
+ * \returns a status code indicating success or failure.
+ *      - STATUS_SUCCESS on success.
+ *      - a non-zero error code on failure.
+ */
+JEMU_SYM(status) JEMU_SYM(j65c02_inst_ORA_imm)(
+    JEMU_SYM(j65c02)* inst, int* cycles);
+
 /******************************************************************************/
 /* Start of public exports.                                                   */
 /******************************************************************************/
