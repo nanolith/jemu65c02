9e5fbe7a1e749357e3a4e96d6618b1f22e3253d1
Justin Handville <nanolith@gmail.com>
2022-12-22 01:47:26 -0500

    impl of PHY instruction.

--- src/instructions.c
+++ src/instructions.c
@@ -206,8 +206,8 @@ JEMU_SYM(j65c02_instruction) JEMU_SYM(global_j65c02_instructions)[256] = {
     { .exec = &JEMU_SYM(j65c02_inst_CLI), .max_cycles = 2 },
     /* Opcode 0x59 - EOR ABS Y instruction. */
     { .exec = &JEMU_SYM(j65c02_inst_EOR_abs_y), .max_cycles = 4 },
-    /* Opcode 0x5A - TODO implement. */
-    { .exec = &JEMU_SYM(j65c02_inst_invalid_opcode), .max_cycles = 2 },
+    /* Opcode 0x5A - PHY instruction. */
+    { .exec = &JEMU_SYM(j65c02_inst_PHY), .max_cycles = 3 },
     /* Opcode 0x5B - TODO implement. */
     { .exec = &JEMU_SYM(j65c02_inst_invalid_opcode), .max_cycles = 2 },
     /* Opcode 0x5C - TODO implement. */
