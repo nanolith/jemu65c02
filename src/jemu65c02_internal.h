d8a6161835466c8aa1ede05e97456d309a7b9004
Justin Handville <nanolith@gmail.com>
2022-12-24 14:49:32 -0500

    impl of the ROL a instruction.

--- src/jemu65c02_internal.h
+++ src/jemu65c02_internal.h
@@ -2128,6 +2128,21 @@ JEMU_SYM(status) JEMU_SYM(j65c02_inst_PLY)(
 void JEMU_SYM(j65c02_op_ROL)(
     JEMU_SYM(j65c02)* inst, uint8_t* val);
 
+/**
+ * \brief Handle a ROL A instruction.
+ *
+ * \param inst              The emulator instance on which this instruction
+ *                          executes.
+ * \param cycles            The number of cycles taken to execute this
+ *                          instruction.
+ *
+ * \returns a status code indicating success or failure.
+ *      - STATUS_SUCCESS on success.
+ *      - a non-zero error code on failure.
+ */
+JEMU_SYM(status) JEMU_SYM(j65c02_inst_ROL_a)(
+    JEMU_SYM(j65c02)* inst, int* cycles);
+
 /******************************************************************************/
 /* Start of public exports.                                                   */
 /******************************************************************************/
