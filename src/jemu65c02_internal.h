3a150ffeafb94acd9bd4999da1cfbc9214471cc8
Justin Handville <nanolith@gmail.com>
2022-12-25 15:07:46 -0500

    impl of ROR zer x instruction.

--- src/jemu65c02_internal.h
+++ src/jemu65c02_internal.h
@@ -2273,6 +2273,21 @@ JEMU_SYM(status) JEMU_SYM(j65c02_inst_ROR_zer)(
 JEMU_SYM(status) JEMU_SYM(j65c02_inst_ROR_abs_x)(
     JEMU_SYM(j65c02)* inst, int* cycles);
 
+/**
+ * \brief Handle a ROR ZER X instruction.
+ *
+ * \param inst              The emulator instance on which this instruction
+ *                          executes.
+ * \param cycles            The number of cycles taken to execute this
+ *                          instruction.
+ *
+ * \returns a status code indicating success or failure.
+ *      - STATUS_SUCCESS on success.
+ *      - a non-zero error code on failure.
+ */
+JEMU_SYM(status) JEMU_SYM(j65c02_inst_ROR_zer_x)(
+    JEMU_SYM(j65c02)* inst, int* cycles);
+
 /******************************************************************************/
 /* Start of public exports.                                                   */
 /******************************************************************************/
