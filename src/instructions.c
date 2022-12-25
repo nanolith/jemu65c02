0fb3eabd4c1aaf0adda6f28014ee8e67bd9416d6
Justin Handville <nanolith@gmail.com>
2022-12-25 14:30:42 -0500

    impl of ROR abs instruction.

--- src/instructions.c
+++ src/instructions.c
@@ -248,8 +248,8 @@ JEMU_SYM(j65c02_instruction) JEMU_SYM(global_j65c02_instructions)[256] = {
     { .exec = &JEMU_SYM(j65c02_inst_JMP_abs_idr), .max_cycles = 6 },
     /* Opcode 0x6D - ADC ABS instruction. */
     { .exec = &JEMU_SYM(j65c02_inst_ADC_abs), .max_cycles = 5 },
-    /* Opcode 0x6E - TODO implement. */
-    { .exec = &JEMU_SYM(j65c02_inst_invalid_opcode), .max_cycles = 2 },
+    /* Opcode 0x6E - ROR ABS instruction. */
+    { .exec = &JEMU_SYM(j65c02_inst_ROR_abs), .max_cycles = 6 },
     /* Opcode 0x6F - TODO implement. */
     { .exec = &JEMU_SYM(j65c02_inst_invalid_opcode), .max_cycles = 2 },
 
