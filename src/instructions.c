d62fcceef13d4dc8f69db3fd8ccba5029b59f201
Justin Handville <nanolith@gmail.com>
2023-01-01 17:39:19 -0500

    impl of STA zer instruction.

--- src/instructions.c
+++ src/instructions.c
@@ -298,8 +298,8 @@ JEMU_SYM(j65c02_instruction) JEMU_SYM(global_j65c02_instructions)[256] = {
     { .exec = &JEMU_SYM(j65c02_inst_invalid_opcode), .max_cycles = 2 },
     /* Opcode 0x84 - TODO implement. */
     { .exec = &JEMU_SYM(j65c02_inst_invalid_opcode), .max_cycles = 2 },
-    /* Opcode 0x85 - TODO implement. */
-    { .exec = &JEMU_SYM(j65c02_inst_invalid_opcode), .max_cycles = 2 },
+    /* Opcode 0x85 - STA ZER instruction. */
+    { .exec = &JEMU_SYM(j65c02_inst_STA_zer), .max_cycles = 3 },
     /* Opcode 0x86 - TODO implement. */
     { .exec = &JEMU_SYM(j65c02_inst_invalid_opcode), .max_cycles = 2 },
     /* Opcode 0x87 - TODO implement. */
