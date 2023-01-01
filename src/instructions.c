36a36bd918c8dcb504125b58305ad428b916a062
Justin Handville <nanolith@gmail.com>
2022-12-31 20:41:38 -0500

    impl of SBC zer idr instruction.

--- src/instructions.c
+++ src/instructions.c
@@ -530,8 +530,8 @@ JEMU_SYM(j65c02_instruction) JEMU_SYM(global_j65c02_instructions)[256] = {
     { .exec = &JEMU_SYM(j65c02_inst_BEQ), .max_cycles = 3 },
     /* Opcode 0xF1 - TODO implement. */
     { .exec = &JEMU_SYM(j65c02_inst_invalid_opcode), .max_cycles = 2 },
-    /* Opcode 0xF2 - TODO implement. */
-    { .exec = &JEMU_SYM(j65c02_inst_invalid_opcode), .max_cycles = 2 },
+    /* Opcode 0xF2 - SBC ZER IDR instruction. */
+    { .exec = &JEMU_SYM(j65c02_inst_SBC_zer_idr), .max_cycles = 6 },
     /* Opcode 0xF3 - TODO implement. */
     { .exec = &JEMU_SYM(j65c02_inst_invalid_opcode), .max_cycles = 2 },
     /* Opcode 0xF4 - TODO implement. */
--- src/jemu65c02_internal.h
+++ src/jemu65c02_internal.h
@@ -2373,6 +2373,21 @@ JEMU_SYM(status) JEMU_SYM(j65c02_inst_SBC_abs)(
 JEMU_SYM(status) JEMU_SYM(j65c02_inst_SBC_zer)(
     JEMU_SYM(j65c02)* inst, int* cycles);
 
+/**
+ * \brief Handle an SBC ZER IDR instruction.
+ *
+ * \param inst              The emulator instance on which this instruction
+ *                          executes.
+ * \param cycles            The number of cycles taken to execute this
+ *                          instruction.
+ *
+ * \returns a status code indicating success or failure.
+ *      - STATUS_SUCCESS on success.
+ *      - a non-zero error code on failure.
+ */
+JEMU_SYM(status) JEMU_SYM(j65c02_inst_SBC_zer_idr)(
+    JEMU_SYM(j65c02)* inst, int* cycles);
+
 /******************************************************************************/
 /* Start of public exports.                                                   */
 /******************************************************************************/
