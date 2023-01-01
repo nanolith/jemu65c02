2c0c79ccdb820910f550e602e5fe7b0f67789e96
Justin Handville <nanolith@gmail.com>
2022-12-31 19:26:38 -0500

    impl of SBC zer instruction.

--- src/jemu65c02_internal.h
+++ src/jemu65c02_internal.h
@@ -2358,6 +2358,21 @@ JEMU_SYM(status) JEMU_SYM(j65c02_inst_SBC_imm)(
 JEMU_SYM(status) JEMU_SYM(j65c02_inst_SBC_abs)(
     JEMU_SYM(j65c02)* inst, int* cycles);
 
+/**
+ * \brief Handle an SBC ZER instruction.
+ *
+ * \param inst              The emulator instance on which this instruction
+ *                          executes.
+ * \param cycles            The number of cycles taken to execute this
+ *                          instruction.
+ *
+ * \returns a status code indicating success or failure.
+ *      - STATUS_SUCCESS on success.
+ *      - a non-zero error code on failure.
+ */
+JEMU_SYM(status) JEMU_SYM(j65c02_inst_SBC_zer)(
+    JEMU_SYM(j65c02)* inst, int* cycles);
+
 /******************************************************************************/
 /* Start of public exports.                                                   */
 /******************************************************************************/
