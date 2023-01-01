b69c4dce435dca5b98e3496444701a9132aa46d5
Justin Handville <nanolith@gmail.com>
2023-01-01 02:09:40 -0500

    impl of the SED instruction.

--- src/instructions.c
+++ src/instructions.c
@@ -542,8 +542,8 @@ JEMU_SYM(j65c02_instruction) JEMU_SYM(global_j65c02_instructions)[256] = {
     { .exec = &JEMU_SYM(j65c02_inst_INC_zer_x), .max_cycles = 6 },
     /* Opcode 0xF7 - TODO implement. */
     { .exec = &JEMU_SYM(j65c02_inst_invalid_opcode), .max_cycles = 2 },
-    /* Opcode 0xF8 - TODO implement. */
-    { .exec = &JEMU_SYM(j65c02_inst_invalid_opcode), .max_cycles = 2 },
+    /* Opcode 0xF8 - SED instruction. */
+    { .exec = &JEMU_SYM(j65c02_inst_SED), .max_cycles = 2 },
     /* Opcode 0xF9 - SBC ABS Y instruction. */
     { .exec = &JEMU_SYM(j65c02_inst_SBC_abs_y), .max_cycles = 5 },
     /* Opcode 0xFA - PLX instruction. */
