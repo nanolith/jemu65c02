d4004393bbfe090acb63dd081680d4ffcad5d3e6
Justin Handville <nanolith@gmail.com>
2023-01-02 01:24:33 -0500

    impl of STA zer x idr.

--- src/instructions.c
+++ src/instructions.c
@@ -290,8 +290,8 @@ JEMU_SYM(j65c02_instruction) JEMU_SYM(global_j65c02_instructions)[256] = {
     /* opcodes 0x80 - 0x8F. */
     /* Opcode 0x80 - BRA instruction. */
     { .exec = &JEMU_SYM(j65c02_inst_BRA), .max_cycles = 3 },
-    /* Opcode 0x81 - TODO implement. */
-    { .exec = &JEMU_SYM(j65c02_inst_invalid_opcode), .max_cycles = 2 },
+    /* Opcode 0x81 - STA ZER X IDR instruction. */
+    { .exec = &JEMU_SYM(j65c02_inst_STA_zer_x_idr), .max_cycles = 6 },
     /* Opcode 0x82 - TODO implement. */
     { .exec = &JEMU_SYM(j65c02_inst_invalid_opcode), .max_cycles = 2 },
     /* Opcode 0x83 - TODO implement. */
