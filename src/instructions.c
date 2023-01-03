4b5e791a2a5ea8ff48b5eab7dddd1179bb1d5b56
Justin Handville <nanolith@gmail.com>
2023-01-03 18:19:36 -0500

    impl of STY abs instruction.

--- src/instructions.c
+++ src/instructions.c
@@ -312,8 +312,8 @@ JEMU_SYM(j65c02_instruction) JEMU_SYM(global_j65c02_instructions)[256] = {
     { .exec = &JEMU_SYM(j65c02_inst_invalid_opcode), .max_cycles = 2 },
     /* Opcode 0x8B - TODO implement. */
     { .exec = &JEMU_SYM(j65c02_inst_invalid_opcode), .max_cycles = 2 },
-    /* Opcode 0x8C - TODO implement. */
-    { .exec = &JEMU_SYM(j65c02_inst_invalid_opcode), .max_cycles = 2 },
+    /* Opcode 0x8C - STY ABS instruction. */
+    { .exec = &JEMU_SYM(j65c02_inst_STY_abs), .max_cycles = 4 },
     /* Opcode 0x8D - STA ABS instruction. */
     { .exec = &JEMU_SYM(j65c02_inst_STA_abs), .max_cycles = 4 },
     /* Opcode 0x8E - STX ABS instruction. */
