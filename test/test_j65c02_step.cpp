e79bcdae35c97fc0b3b668558f23e558b5f84c2e
Justin Handville <nanolith@gmail.com>
2023-01-02 19:59:06 -0500

    Don't step when the processor is stopped.

--- test/test_j65c02_step.cpp
+++ test/test_j65c02_step.cpp
@@ -2,6 +2,8 @@
 #include <jemu65c02/jemu65c02.h>
 #include <string.h>
 
+#include "../src/jemu65c02_internal.h"
+
 JEMU_IMPORT_jemu65c02;
 
 TEST_SUITE(j65c02_step);
@@ -67,6 +69,60 @@ TEST(step_NOP)
 
     /* POSTCONDITION: crash flag is not set. */
     TEST_EXPECT(!j65c02_crash_flag_get(inst));
+
+    /* clean up. */
+    TEST_ASSERT(STATUS_SUCCESS == j65c02_release(inst));
+}
+
+/**
+ * Attempt to step processor while it is stopped returns a specific error.
+ */
+TEST(step_stopped)
+{
+    j65c02* inst = nullptr;
+    uint8_t mem[65536];
+
+    /* clear memory. */
+    memset(mem, 0, sizeof(mem));
+
+    /* set the reset vector. */
+    mem[0xFFFC] = 0x00;
+    mem[0xFFFD] = 0x10;
+
+    /* at 0x1000, add a NOP instruction. */
+    mem[0x1000] = 0xEA;
+
+    /* create an instance. */
+    TEST_ASSERT(
+        STATUS_SUCCESS
+            == j65c02_create(
+                    &inst, &mem_read, &mem_write, mem,
+                    JEMU_65c02_PERSONALITY_WDC,
+                    JEMU_65c02_EMULATION_MODE_STRICT));
+
+    /* PRECONDITION: crash flag is set. */
+    TEST_EXPECT(j65c02_crash_flag_get(inst));
+
+    /* reset the processor. */
+    TEST_ASSERT(STATUS_SUCCESS == j65c02_reset(inst));
+
+    /* PRECONDITION: PC is 0x1000. */
+    TEST_ASSERT(0x1000 == j65c02_reg_pc_get(inst));
+
+    /* PRECONDITION: processor is stopped. */
+    inst->stopped = true;
+
+    /* single step. */
+    TEST_ASSERT(JEMU_ERROR_PROCESSOR_STOPPED == j65c02_step(inst));
+
+    /* POSTCONDITION: PC is 0x1000. */
+    TEST_EXPECT(0x1000 == j65c02_reg_pc_get(inst));
+
+    /* POSTCONDITION: crash flag is not set. */
+    TEST_EXPECT(!j65c02_crash_flag_get(inst));
+
+    /* POSTCONDITION: stopped flag is set. */
+    TEST_EXPECT(j65c02_stopped_flag_get(inst));
 
     /* clean up. */
     TEST_ASSERT(STATUS_SUCCESS == j65c02_release(inst));
