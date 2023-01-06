3f7190b31e6f16d6750d3a0b77a43ea7491c52b0
Justin Handville <nanolith@gmail.com>
2023-01-05 20:52:44 -0500

    impl of TSB zer instruction.

--- src/instructions.c
+++ src/instructions.c
@@ -24,8 +24,8 @@ JEMU_SYM(j65c02_instruction) JEMU_SYM(global_j65c02_instructions)[256] = {
     { .exec = &JEMU_SYM(j65c02_inst_invalid_opcode), .max_cycles = 2 },
     /* Opcode 0x03 - TODO implement. */
     { .exec = &JEMU_SYM(j65c02_inst_invalid_opcode), .max_cycles = 2 },
-    /* Opcode 0x04 - TODO implement. */
-    { .exec = &JEMU_SYM(j65c02_inst_invalid_opcode), .max_cycles = 2 },
+    /* Opcode 0x04 - TSB ZER instruction. */
+    { .exec = &JEMU_SYM(j65c02_inst_TSB_zer), .max_cycles = 5 },
     /* Opcode 0x05 - ORA ZER instruction. */
     { .exec = &JEMU_SYM(j65c02_inst_ORA_zer), .max_cycles = 3 },
     /* Opcode 0x06 - ASL ZER instruction. */
---
+++ test/test_TSB_zer.cpp
@@ -0,0 +1,155 @@
+#include <minunit/minunit.h>
+#include <jemu65c02/jemu65c02.h>
+#include <string.h>
+
+JEMU_IMPORT_jemu65c02;
+
+TEST_SUITE(j65c02_TSB_zer);
+
+static status mem_read(void* varr, uint16_t addr, uint8_t* val)
+{
+    const uint8_t* arr = (const uint8_t*)varr;
+
+    *val = arr[addr];
+
+    return STATUS_SUCCESS;
+}
+
+static status mem_write(void* varr, uint16_t addr, uint8_t val)
+{
+    uint8_t* arr = (uint8_t*)varr;
+
+    arr[addr] = val;
+
+    return STATUS_SUCCESS;
+}
+
+/**
+ * 0x04 TSB zer instruction normal operation.
+ */
+TEST(step_TSB_zer)
+{
+    j65c02* inst = nullptr;
+    uint8_t mem[65536];
+    const uint8_t EXPECTED_A_VALUE = 0x81;
+    const uint8_t EXPECTED_INPUT = 0x7F;
+    const uint8_t EXPECTED_OUTPUT = 0xFF;
+
+    /* clear memory. */
+    memset(mem, 0, sizeof(mem));
+
+    /* set the reset vector. */
+    mem[0xFFFC] = 0x00;
+    mem[0xFFFD] = 0x10;
+
+    /* at 0x1000, add a TSB. */
+    mem[0x1000] = 0x04;
+    mem[0x1001] = 0x05;
+
+    /* at 0x0005, set the input. */
+    mem[0x0005] = EXPECTED_INPUT;
+
+    /* create an instance. */
+    TEST_ASSERT(
+        STATUS_SUCCESS
+            == j65c02_create(
+                    &inst, &mem_read, &mem_write, mem,
+                    JEMU_65c02_PERSONALITY_WDC,
+                    JEMU_65c02_EMULATION_MODE_STRICT));
+
+    /* PRECONDITION: crash flag is set. */
+    TEST_EXPECT(j65c02_crash_flag_get(inst));
+
+    /* reset the processor. */
+    TEST_ASSERT(STATUS_SUCCESS == j65c02_reset(inst));
+
+    /* PRECONDITION: set A to the expected A value. */
+    j65c02_reg_a_set(inst, EXPECTED_A_VALUE);
+
+    /* PRECONDITION: zero flag is set. */
+    j65c02_reg_status_set(
+        inst, j65c02_reg_status_get(inst) | JEMU_65c02_STATUS_ZERO);
+
+    /* PRECONDITION: PC is 0x1000. */
+    TEST_ASSERT(0x1000 == j65c02_reg_pc_get(inst));
+
+    /* single step. */
+    TEST_ASSERT(STATUS_SUCCESS == j65c02_step(inst));
+
+    /* POSTCONDITION: PC is 0x1002. */
+    TEST_EXPECT(0x1002 == j65c02_reg_pc_get(inst));
+
+    /* POSTCONDITION: mem[0x0005] is EXPECTED_OUTPUT. */
+    TEST_EXPECT(EXPECTED_OUTPUT == mem[0x0005]);
+
+    /* POSTCONDITION: zero flag is clear. */
+    TEST_EXPECT(!(j65c02_reg_status_get(inst) & JEMU_65c02_STATUS_ZERO));
+
+    /* clean up. */
+    TEST_ASSERT(STATUS_SUCCESS == j65c02_release(inst));
+}
+
+/**
+ * 0x04 TSB zer instruction normal operation.
+ */
+TEST(step_TSB_zer_zero)
+{
+    j65c02* inst = nullptr;
+    uint8_t mem[65536];
+    const uint8_t EXPECTED_A_VALUE = 0x80;
+    const uint8_t EXPECTED_INPUT = 0x0F;
+    const uint8_t EXPECTED_OUTPUT = 0x8F;
+
+    /* clear memory. */
+    memset(mem, 0, sizeof(mem));
+
+    /* set the reset vector. */
+    mem[0xFFFC] = 0x00;
+    mem[0xFFFD] = 0x10;
+
+    /* at 0x1000, add a TSB. */
+    mem[0x1000] = 0x04;
+    mem[0x1001] = 0x05;
+
+    /* at 0x0005, set the input. */
+    mem[0x0005] = EXPECTED_INPUT;
+
+    /* create an instance. */
+    TEST_ASSERT(
+        STATUS_SUCCESS
+            == j65c02_create(
+                    &inst, &mem_read, &mem_write, mem,
+                    JEMU_65c02_PERSONALITY_WDC,
+                    JEMU_65c02_EMULATION_MODE_STRICT));
+
+    /* PRECONDITION: crash flag is set. */
+    TEST_EXPECT(j65c02_crash_flag_get(inst));
+
+    /* reset the processor. */
+    TEST_ASSERT(STATUS_SUCCESS == j65c02_reset(inst));
+
+    /* PRECONDITION: set A to the expected A value. */
+    j65c02_reg_a_set(inst, EXPECTED_A_VALUE);
+
+    /* PRECONDITION: zero flag is clear. */
+    j65c02_reg_status_set(
+        inst, j65c02_reg_status_get(inst) & ~JEMU_65c02_STATUS_ZERO);
+
+    /* PRECONDITION: PC is 0x1000. */
+    TEST_ASSERT(0x1000 == j65c02_reg_pc_get(inst));
+
+    /* single step. */
+    TEST_ASSERT(STATUS_SUCCESS == j65c02_step(inst));
+
+    /* POSTCONDITION: PC is 0x1002. */
+    TEST_EXPECT(0x1002 == j65c02_reg_pc_get(inst));
+
+    /* POSTCONDITION: mem[0x0005] is EXPECTED_OUTPUT. */
+    TEST_EXPECT(EXPECTED_OUTPUT == mem[0x0005]);
+
+    /* POSTCONDITION: zero flag is set. */
+    TEST_EXPECT(j65c02_reg_status_get(inst) & JEMU_65c02_STATUS_ZERO);
+
+    /* clean up. */
+    TEST_ASSERT(STATUS_SUCCESS == j65c02_release(inst));
+}
