67d194bf8a23717a7ee5c9e60798b68a1f46bfd6
Justin Handville <nanolith@gmail.com>
2023-01-06 16:50:47 -0500

    impl of the TXA instruction.

---
+++ src/j65c02_inst_TXA.c
@@ -0,0 +1,57 @@
+/**
+ * \file j65c02_inst_TXA.c
+ *
+ * \brief Handle a TXA instruction.
+ *
+ * \copyright 2023 Justin Handville.  Please see LICENSE.txt in this
+ * distribution for the license terms under which this software is distributed.
+ */
+
+#include "jemu65c02_internal.h"
+
+JEMU_IMPORT_jemu65c02;
+JEMU_IMPORT_jemu65c02_internal;
+
+/**
+ * \brief Handle a TXA instruction.
+ *
+ * \param inst              The emulator instance on which this instruction
+ *                          executes.
+ * \param cycles            The number of cycles taken to execute this
+ *                          instruction.
+ *
+ * \returns a status code indicating success or failure.
+ *      - STATUS_SUCCESS on success.
+ *      - a non-zero error code on failure.
+ */
+JEMU_SYM(status) JEMU_SYM(j65c02_inst_TXA)(
+    JEMU_SYM(j65c02)* inst, int* cycles)
+{
+    /* set A to X. */
+    inst->reg_a = inst->reg_x;
+
+    /* set the negative flag. */
+    if (inst->reg_a & 0x80)
+    {
+        inst->reg_status |= JEMU_65c02_STATUS_NEGATIVE;
+    }
+    else
+    {
+        inst->reg_status &= ~JEMU_65c02_STATUS_NEGATIVE;
+    }
+
+    /* set the zero flag. */
+    if (0x00 == inst->reg_a)
+    {
+        inst->reg_status |= JEMU_65c02_STATUS_ZERO;
+    }
+    else
+    {
+        inst->reg_status &= ~JEMU_65c02_STATUS_ZERO;
+    }
+
+    /* this instruction takes 2 cycles. */
+    *cycles = 2;
+
+    return STATUS_SUCCESS;
+}
